9041812eeccbf07128a3e23f5844b14af62e2118
Axel Nunez <xlnunez317@gmail.com>
2020-03-28 20:41:26 -0400

    AppDelegate, SceneDelegate, project.pbxproj, ViewController: Updating for backward compatibility with iOS 12

--- YFN/AppDelegate.swift
+++ YFN/AppDelegate.swift
@@ -12,21 +12,33 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        navigationController = UINavigationController(rootViewController: ViewController())
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.navigationBar.isTranslucent = true
+        
+        let keyWindow = UIWindow(frame: UIScreen.main.bounds)
+        keyWindow.rootViewController = navigationController
+        keyWindow.makeKeyAndVisible()
+        window = keyWindow
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
