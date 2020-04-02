cbd3697c9473515abdf2972eea5d30d835f4da35
Axel Nunez <xlnunez317@gmail.com>
2020-04-02 12:22:14 -0400

    Adding calendar view and date cell

---
+++ YFN/Views/CalendarView/DateCell.swift
@@ -0,0 +1,19 @@
+//
+//  DateCell.swift
+//  YFN
+//
+//  Created by Axel Nunez on 3/30/20.
+//  Copyright © 2020 Axel Nunez. All rights reserved.
+//
+
+import UIKit
+
+class DateCell: UICollectionViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
