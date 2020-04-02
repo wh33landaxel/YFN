310528c5bb4cf8ab605f9a7e7e90fb474b2507eb
Axel Nunez <xlnunez317@gmail.com>
2020-04-02 14:42:54 -0400

    Adding artist table view cells

---
+++ YFN/Views/Cells/ArtistTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistTableViewCell.swift
+//  YFN
+//
+//  Created by Axel Nunez on 4/2/20.
+//  Copyright © 2020 Axel Nunez. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
