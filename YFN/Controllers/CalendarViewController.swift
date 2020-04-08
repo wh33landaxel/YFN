5a025a9ee12105f26393531e11638fc2c8a3f1e2
Axel Nunez <xlnunez317@gmail.com>
2020-04-07 21:05:12 -0400

    CalendarViewController: dimming out dates

--- YFN/Controllers/CalendarViewController.swift
+++ YFN/Controllers/CalendarViewController.swift
@@ -43,13 +43,26 @@ extension CalendarViewController: JTACMonthViewDataSource {
 
 extension CalendarViewController: JTACMonthViewDelegate {
     func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        let cell = cell as! DateCell
+        configureCell(view: cell, cellState: cellState)
+    }
+    
+    func configureCell(view: JTACDayCell?, cellState: CellState) {
+        guard let cell = view as? DateCell else { return }
         cell.dateLabel.text = cellState.text
+        handleCellTextColor(cell: cell, cellState: cellState)
+    }
+    
+    func handleCellTextColor(cell: DateCell, cellState: CellState) {
+        if cellState.dateBelongsTo == .thisMonth {
+            cell.dateLabel.textColor = UIColor.black
+        } else {
+            cell.dateLabel.textColor = UIColor.gray
+        }
     }
     
     func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
-        cell.dateLabel.text = cellState.text
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
         return cell
     }
     
